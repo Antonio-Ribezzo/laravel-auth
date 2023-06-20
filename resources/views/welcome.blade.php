b6f1eccbbf584839443f289d5a948436943707f6
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-20 22:28:12 +0200

    Style welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
+
+@section('title')
+Bezz
+@endsection
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
+{{-- <div class="jumbotron p-5 mb-4 rounded-3">
     <div class="container py-5">
         <div class="logo_laravel">
             <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
@@ -17,11 +21,14 @@
         <p class="col-md-8 fs-4">This a preset package with Bootstrap 5 views for laravel projects including laravel breeze/blade. It works from laravel 9.x to the latest release 10.x</p>
         <a href="https://packagist.org/packages/pacificdev/laravel_9_preset" class="btn btn-primary btn-lg" type="button">Documentation</a>
     </div>
-</div>
-
-<div class="content">
-    <div class="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
+</div> --}}
+<div id="jumbotron" class="container">
+    <h1 class="text-capitalize">creative & technical<br>Web Developer</h1>
+    <hr>
+    <div class="d-flex mt-3 justify-content-start align-items-center">
+        <h2 class="text-capitalize">antonio ribezzo</h2>
+        <span class="mx-3">-</span>
+        <span>Portfolio</span>
     </div>
 </div>
 @endsection
