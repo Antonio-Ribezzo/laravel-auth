d2b771cef673c099a3c788ef965ef50a96d63ef2
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-06-21 02:46:44 +0200

    CRUD (destroy)

--- resources/views/admin/pages/projects/show.blade.php
+++ resources/views/admin/pages/projects/show.blade.php
@@ -7,7 +7,14 @@
         <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Buyer</span>: {{ $project->buyer }}</h6>
         <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Technologies</span>: {{ $project->programming_languages }}</h6>
         <h6 class="card-subtitle mb-2 text-body-secondary"><span class="text-decoration-underline">Link</span>:<a href="#" class="ms-1 card-link">{{$project->link}}</a></h6>
+        <div class="d-flex mt-3">
+            <a class="me-2 btn btn-outline-light" href="{{route('admin.project.edit',$project)}}">Edit Project</a>
+            <form action="{{route('admin.project.destroy', $project)}}" method="POST">
+                @csrf
+                @method('DELETE')
 
-        <a class="mt-3 btn btn-outline-light" href="{{route('admin.project.edit',$project)}}">Edit Project</a>
+                <button class="btn btn-outline-danger" type="submit" onclick="return confirm('Are you sure you want to delete the project?')">Delete Project</button>
+            </form>
+        </div>
 </div>
 @endsection
